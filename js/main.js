5b0e1445a79e734aabce8ec9d505ad73c6dfbb50
ryanpetersonOF <ryan@openfin.co>
2017-08-09 14:29:15 +0100

    added back io connector

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var socket = io("http://localhost:8080");
+var socket = io(window.location.protocol+"//"+window.location.hostname+":8080");
 var username = "Anonymous";
 var myTurn = false;
 var userCount;
